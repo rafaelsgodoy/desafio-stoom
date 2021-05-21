0afbd8594cf6b671a2c13ad6256031b74e1066b6
Rafael Godoy <rafaelsgodoy@gmail.com>
2021-05-21 17:57:45 -0300

    container cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -12,24 +12,26 @@ const Cart = () => {
     <>
       <Header />
       <Box height={60} width={1}></Box>
-      <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-        <Box width={240} height={240} >
-          <Box style={{
-            width: 240,
-            height: 240,
-            position: 'absolute',
-            borderRadius: '50%',
-            backgroundColor: grey[200]
-          }}>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Box style={{ minWidth: 400, width: '100%', maxWidth: 900 }} flexDirection="column" display="flex" justifyContent="center" alignItems="center">
+          <Box width={240} height={240} >
+            <Box style={{
+              width: 240,
+              height: 240,
+              position: 'absolute',
+              borderRadius: '50%',
+              backgroundColor: grey[200]
+            }}>
+            </Box>
+            <Box position="absolute">
+              <DeliverySvg />
+            </Box>
           </Box>
-          <Box position="absolute">
-            <DeliverySvg />
-          </Box>
+
+          <EmptyCart cart={cart} />
+          <ListCart cart={cart} />
+
         </Box>
-
-        <EmptyCart cart={cart} />
-        <ListCart cart={cart} />
-
       </Box>
     </>
   )
